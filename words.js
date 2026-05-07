3bb8f3dfe5b06b633ca5ee4245872d1f1bac2608
BongKM1130 <asddwda4@gmail.com>
2026-05-07 15:56:52 +0900

    Update words.js

--- words.js
+++ words.js
@@ -1,41 +1,7 @@
 const words = [
-    {
-        english: "executive",
-        meaning: "경영진"
-    },
-
-    {
-        english: "inventory",
-        meaning: "재고"
-    },
-
-    {
-        english: "feature",
-        meaning: "특징"
-    },
-
-    {
-        english: "refund",
-        meaning: "환불"
-    },
-
-    {
-        english: "reserve",
-        meaning: "예약하다"
-    },
-
-    {
-        english: "revenue",
-        meaning: "수익"
-    },
-
-    {
-        english: "enhance",
-        meaning: "향상시키다"
-    },
-
-    {
-        english: "candidate",
-        meaning: "후보자"
-    }
+    { english: "executive", meaning: "경영진" },
+    { english: "inventory", meaning: "재고" },
+    { english: "feature", meaning: "특징" },
+    { english: "refund", meaning: "환불" },
+    { english: "reserve", meaning: "예약하다" }
 ];
